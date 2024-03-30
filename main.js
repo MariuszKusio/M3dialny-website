1da6380d77b4442bf3338a7571141bf585c868c6
Mariusz Kusio <kusio1996@gmail.com>
2024-03-30 17:44:26 +0100

    update

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ sliderContainer.addEventListener('mousedown', (e) => {
   pressed = true;
   startx = e.offsetX - slider.offsetLeft;
   // sliderContainer.style.cursor = 'grabbing';
-  sliderContainer.style.cursor = 'url("img/cursor1.svg"), auto';
+  sliderContainer.style.cursor = 'url("img/cursor1opacity.svg"), auto';
 });
 
 sliderContainer.addEventListener('mouseenter', () => {
@@ -53,10 +53,10 @@ sliderContainer.addEventListener('mouseenter', () => {
   });
 
   // Zrób tak żeby przycisk zobacz więcej na realizacjach po kliknięciu nie przesuwał slidera bo robi błędy
-  realizationButton.forEach(item => item.addEventListener('click', () => {
-    console.log('klikam przycisk');
-    pressed = false ;
-  }));
+  // realizationButton.forEach(item => item.addEventListener('click', () => {
+  //   console.log('klikam przycisk');
+  //   pressed = false ;
+  // }));
 
   sliderContainer.addEventListener('mousemove', (e) => {
      if(!pressed) return;
@@ -64,18 +64,14 @@ sliderContainer.addEventListener('mouseenter', () => {
      x = e.offsetX;
 
      slider.style.left = `${x - startx}px`;
-    //  console.log(x - startx);
+   
 
     checkBoundary()
+
   });
 
 
   function checkBoundary() {
-    let outer = sliderContainer.getBoundingClientRect();
-    let inner = slider.getBoundingClientRect();
- 
-    slider.style.left = `-${sliderContainer.offsetWidth - totalCardsSliderWidth}px`;
-
 
     // safety about get under -30px left slider container value and more left px than slider width
     if(parseInt(slider.style.left) > 40) {
@@ -84,11 +80,69 @@ sliderContainer.addEventListener('mouseenter', () => {
         slider.style.left = `${sliderContainer.offsetWidth - totalCardsSliderWidth}px`;
     }
 
-    // console.log(`${parseInt(slider.style.left)} > ${sliderContainer.offsetWidth - totalCardsSliderWidth}` )
-    // console.log(parseInt(slider.style.left))
   }
 
+
+
+  // Gallery 
+
+  const realizationGallery = document.querySelector(".realizationsGallery");
+  const closeButton = document.querySelector(".closeButton");
   
+  // Close gallery
+  closeButton.addEventListener('click', () => {
+   realizationGallery.style.display = 'none';
+   document.body.style.overflow = '';
+  });
+
+  // Show gallery
+
+  realizationButton.forEach( button => {
+    button.addEventListener('click', (e) => {
+      realizationGallery.style.display = 'block';
+      document.body.style.overflow = 'hidden';
+      console.log(e.target.id);
+    })
+  });
+
+  
+// Delayed animations
+
+// header animation
+
+const firstVideo = document.querySelector(".firstVideoContainer");
+const secondVideo = document.querySelector(".secontVideoContainer");
+// Pobierz video ze strony głównej żeby potem po jego załadowaniu odpalić animacje. Pobierzemy tutaj video, które jest pod spodem żęby na pewno pobrało się
+// razem z tym drugim video, które też dotyczy tej animacji
+const firstVideoLoadClass = document.querySelector(".firstVideo");
+const headerTitle = document.querySelector(".headerTitle");
+
+firstVideoLoadClass.addEventListener('load', function() {
+
+  setTimeout(() => {
+    firstVideo.classList.add('firstMove');
+    secondVideo.classList.add('secondMove');
+  }, 1000)
+  
+})
+
+let scrollCounter = 0;
+
+window.addEventListener('scroll', function() {
+  scrollCounter++;
+  // safety about auto entry scroll
+  if(scrollCounter > 1){
+  //Desktop animation
+  firstVideo.classList.add('firstMove');
+  secondVideo.classList.add('secondMove');
+
+
+  //Mobile animation
+  headerTitle.classList.add('moveTitle');
+  }
+});
+
+
 
 
 
