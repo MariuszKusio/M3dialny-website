955329d208c04c1d26137b8894a075b99462d82e
MariuszKusio <91279272+MariuszKusio@users.noreply.github.com>
2024-04-05 12:08:46 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,189 +1,186 @@
-// Components from html
+// Realizations section nav button
 
-
-// Message Component
-const emailButton = document.querySelector('.sendMessage');
-let emailButtonFlag = true;   // flag for make button disable after sending message (anti-spam)
-let formUserName = document.querySelector('.userNameInput');
-let formSubject = document.querySelector('.subjectInput');
-let formReplyTo = document.querySelector('.userEmailInput');
-let formMessage = document.querySelector('.textMessage');
-const validationLabel = document.querySelector('.formValidationLabel');
-
-// Gallery Component
-const selectAllButton = document.querySelector('.selectAll');
-const selectPhotosButton = document.querySelector('.selectPhotos');
-const selectVideosButton = document.querySelector('.selectVideos');
-// na razie nie wiem czy to potrzebne, ale wybierzemy sobie wszystkie przyciski z daną klasą
-
-const selectButtons = document.querySelectorAll('.filterPick li:nth-child(n+2)');
-const videoItems = document.querySelectorAll('.mainGallery video')
-
-
-
-
-
-
-// Messages
-
-// this function make message button disabled for 20 seconds and put text in validation label when cooldown will ended
-const disabledButton = () => {
-   emailButton.disabled = true;
-   
-   setTimeout(() => {
-    validationLabel.textContent = 'Możesz wysłać wiadomość.';
-    emailButton.disabled = false;
-    emailButtonFlag = true; 
-   }, 20000);
-}
-
-// main function to send message with secure of spam
-const sendEmail = () => {
-    (function(){
-      emailjs.init("jQrZDq8T5rFR7ZI3A");
-    })();
-    
-    const myEmail = 'djlucano83@gmail.com'; // email to get messages from the contact form
-
-    // const myEmail = 'mariuszkusio33@gmail.com';
-    // parameters for emailJS 
-    let params = {
-       senderName: formUserName.value,
-       subject: formSubject.value,
-       to: myEmail,
-       replyTo: formReplyTo.value,
-       message: formMessage.value,
-
-    };      
-
-    // safeguard for email messages
-    if(emailButtonFlag === true){
-
-    if ([...params.senderName].length < 1) {
-        validationLabel.textContent = 'Podaj imię';
-    } else if ([...params.senderName].length > 15) {
-        validationLabel.textContent = 'Za długa nazwa';
-    }
-    else if ([...params.replyTo].length < 1) {
-        validationLabel.textContent = 'Podaj email';
-    } else if ([...params.replyTo].length > 60) {
-        validationLabel.textContent = 'Za długi email';
-    }
-    else if ([...params.subject].length < 1) {
-        validationLabel.textContent = 'Podaj temat';
-    } else if ([...params.subject].length > 80) {
-        validationLabel.textContent = 'Za długi temat';
-    } 
-     else if ([...params.message].length < 1) {
-        validationLabel.textContent = 'Napisz wiadomość';
-    } else if ([...params.subject].length > 2000) {
-        validationLabel.textContent = 'Za długa wiadomość(max 2000 znaków)';
-    }
-    else {
-        let serviceID = "service_f0figcn";
-        let templateID = "template_jghr7s1";
-    
-        emailjs.send("service_f0figcn","template_jghr7s1", params)
-        .then( res => {
-          validationLabel.textContent = 'Wiadomość została wysłana!';
-          // clear form
-          formMessage.value = '';
-          formReplyTo.value = '';
-          formUserName.value = '';
-          formSubject.value = '';
-          
-          emailButtonFlag = false;  
-        })
-        .catch();
-    }
-
-} else if(emailButtonFlag === false) {
-    validationLabel.textContent = 'Poczekaj 20 sekund.';
-
-    disabledButton(); 
-}
-}
-
-
-emailButton.addEventListener('click', sendEmail);
-
-
-
-// Gallery
-
-
-// Gallery Component
-const showAllButton = document.getElementById('showAll');
-const showPhotosButton = document.getElementById('showPhotos');
-const showVideosButton = document.getElementById('showVideos');
-
-const gallery = document.getElementById('gallery');
-const pickButtons = document.getElementById('pickButtons');
-
-
-
-
-showAllButton.addEventListener('click', function() {showItems('all')});
-showPhotosButton.addEventListener('click', function() {showItems('photoItem')});
-showVideosButton.addEventListener('click', function() {showItems('videoItem')});
-
-
-// Filter photos and video
-function showItems(type) {
-    // underline button change when we pick some filter option.
-    const buttons = Array.from(pickButtons.children);
-    buttons.forEach(button => {
-        button.classList.remove('filterSelected');
-
-        if(showAllButton.dataset.btntype == type) {
-            showAllButton.classList.add('filterSelected')
-        } else if(showPhotosButton.dataset.btntype == type) {
-            showPhotosButton.classList.add('filterSelected')
-        } else if(showVideosButton.dataset.btntype == type) {
-            showVideosButton.classList.add('filterSelected')
-        }
-    })
-    
-
-    // Filter make by Array with gallery items. 
-    const items = Array.from(gallery.children);
-    items.forEach(item => {
-        const isVisible = type === 'all' || item.classList.contains(type);
-        item.style.display = isVisible ? 'block' : 'none';
-       
-        // Conditional statement for filter item in lightbox. When element in DOM don't have data-dimbox = 'lucano-Gallery' it will not be shown in lightbox preview
-        if( type == 'all' && item.dataset.dimbox !== 'lucano-Gallery' ){
-            item.dataset.dimbox = 'lucano-Gallery';
-        } else if ( type == 'photoItem') {
-            item.classList.contains('videoItem') ? item.dataset.dimbox = '' : item.dataset.dimbox = 'lucano-Gallery';
-            item.classList.contains('photoItem') ? item.dataset.dimbox = 'lucano-Gallery' : item.dataset.dimbox = '';
-        } else if ( type == 'videoItem'  ){
-            item.classList.contains('videoItem') ? item.dataset.dimbox = 'lucano-Gallery' : item.dataset.dimbox = '';
-            item.classList.contains('photoItem') ? item.dataset.dimbox = '' : item.dataset.dimbox = 'lucano-Gallery';
-        }
-        
-    })
-
-}
-
-
-
-// Website main navigation buttons
-
-// Menu buttons
-const menuGalleryBtn = document.getElementById('menuGalleryBtn');
-const menuAboutBtn = document.getElementById('menuAboutBtn');
-const menuContactBtn = document.getElementById('menuContactBtn');
-
+const realizationNavButton = document.getElementById('projectsShowButton');
+const realizationNavButtonMobile = document.getElementById('projectsShowButtonMobile');
 
 function scrollToSection(sectionId) {
-  const section = document.getElementById(sectionId);
+    const section = document.getElementById(sectionId);
 
-  if (section) {
-    section.scrollIntoView({ behavior:'smooth' });
-  }
+    if (section) {
+        section.scrollIntoView({ behavior:'smooth' })
+    }
 }
 
-menuGalleryBtn.addEventListener('click', function() { scrollToSection('gallerySection') });
-menuAboutBtn.addEventListener('click', function() { scrollToSection('aboutSection') });
-menuContactBtn.addEventListener('click', function() { scrollToSection('contactSection') });
+realizationNavButton.addEventListener('click', function() { scrollToSection('realizations') });
+realizationNavButtonMobile.addEventListener('click', function() { scrollToSection('realizations') });
+
+
+
+
+// Realizations slider 
+const sliderContainer = document.querySelector('.realizationsPositionContainer');
+const slider = document.querySelector('.realizationsContainer');
+const slide = document.querySelectorAll('.realizationCard');
+const realizationButton = document.querySelectorAll('.realizationShowButton');
+
+
+let pressed = false;
+let startx;
+let x;
+
+let totalCardsSliderWidth = (slide[0].offsetWidth + 40)  * slide.length;
+
+sliderContainer.addEventListener('mousedown', (e) => {
+  pressed = true;
+  startx = e.offsetX - slider.offsetLeft;
+  // custom cursor
+  sliderContainer.style.cursor = 'url("img/cursor1opacity.svg"), auto';
+});
+
+sliderContainer.addEventListener('mouseenter', () => {
+ 
+    sliderContainer.style.cursor = 'url("img/cursor1.svg"), auto';
+  
+  });
+
+  sliderContainer.addEventListener('mouseup', () => {
+    sliderContainer.style.cursor = 'url("img/cursor1.svg"), auto';
+  });
+
+  window.addEventListener('mouseup', () => {
+    pressed = false;
+  });
+
+  // Zrób tak żeby przycisk zobacz więcej na realizacjach po kliknięciu nie przesuwał slidera bo robi błędy
+  realizationButton.forEach(item => item.addEventListener('click', () => {
+    console.log('klikam przycisk');
+    pressed = false ;
+  }));
+
+  sliderContainer.addEventListener('mousemove', (e) => {
+     if(!pressed) return;
+     e.preventDefault();
+     x = e.offsetX;
+
+     slider.style.left = `${x - startx}px`;
+
+    checkBoundary()
+  });
+
+
+  function checkBoundary() {
+
+    // safety about get under 40px left slider container value and more left px than slider width
+    if(parseInt(slider.style.left) > 40) {
+        slider.style.left = '40px'
+    } else if(parseInt(slider.style.left) < (sliderContainer.offsetWidth - totalCardsSliderWidth )) {
+        slider.style.left = `${sliderContainer.offsetWidth - totalCardsSliderWidth}px`;
+    }
+
+  }
+
+
+
+  // Gallery 
+
+  const realizationGallery = document.querySelector(".realizationsGallery");
+  const closeButton = document.querySelector(".closeButton");
+  
+  // Close gallery
+  closeButton.addEventListener('click', () => {
+   realizationGallery.style.display = 'none';
+   document.body.classList.remove('disabledScroll');
+  });
+
+  // Show gallery
+
+  realizationButton.forEach( button => {
+    button.addEventListener('click', (e) => {
+      realizationGallery.style.display = 'block';
+      document.body.classList.add('disabledScroll');
+      // console.log(e.target.id);
+    })
+  });
+
+// header animation
+
+const firstVideo = document.querySelector(".firstVideoContainer");
+const secondVideo = document.querySelector(".secontVideoContainer");
+const firstVideoLoad = document.querySelector(".firstVideo");
+const secondVideoLoad = document.querySelector(".secondVideo");
+const headerTitle = document.querySelector(".headerTitle");
+
+firstVideoLoad.addEventListener('load', function() {
+
+  setTimeout(() => {
+    firstVideo.classList.add('firstMove');
+    secondVideo.classList.add('secondMove');
+  }, 1000)
+  
+})
+
+let scrollCounter = 0;
+
+window.addEventListener('scroll', function() {
+  scrollCounter++;
+  // safety about auto entry scroll
+  if(scrollCounter > 1){
+  //Desktop animation
+  firstVideo.classList.add('firstMove');
+  secondVideo.classList.add('secondMove');
+
+  //Mobile animation
+  headerTitle.classList.add('moveTitle');
+  }
+});
+
+// longer video with the same file size
+firstVideoLoad.playbackRate = 0.8;
+secondVideoLoad.playbackRate = 0.8;
+
+
+const loader = document.querySelector('.loader');
+const loaderLogo = document.querySelector('.loadAnimation img');
+const vanishBlock = document.querySelector('.vanishBlock');
+
+// loading page listener with vanish function
+window.addEventListener('load', () => {
+  vanishBlock.style.display = 'none';
+  loader.style.transition = '.9s';
+  loaderLogo.style.transition = '.6s';
+  loader.classList.add('fadeOut');
+  window.scrollY === 0 ? loaderLogo.classList.add('fadeOut') : loaderLogo.style.top = '-200px';
+
+  
+
+  setTimeout( () => {
+    loader.style.display = "none";
+    document.body.classList.remove('disabledScroll');
+  }, 1000);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
